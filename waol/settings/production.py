f75e3d43d69c30c76eb23278db675bb9754de60f
Hektor Wallin <hektorw@gmail.com>
2015-09-19 17:45:44 +0200

    Added heroku config to serve static files, maybeee

--- waol/settings/production.py
+++ waol/settings/production.py
@@ -26,6 +26,17 @@ ALLOWED_HOSTS = ['*']
 
 
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
+
+
 try:
     from .local import *
 except ImportError:
